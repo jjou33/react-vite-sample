2e611804bacd0105be9502df4e9e5a0afbafd8de
jjou33 <zanda33@naver.com>
2022-09-05 00:02:39 +0900

    STYLE: eslint 및 prettier 설정

---
+++ .eslintrc.cjs
@@ -0,0 +1,52 @@
+/**
+ * ESLint configuration.
+ *
+ * @see https://eslint.org/docs/user-guide/configuring
+ */
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+    browser: true,
+    node: true
+  },
+  parser: '@typescript-eslint/parser',
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  parserOptions: {
+    ecmaFeatures: { jsx: true },
+    ecmaVersion: 'lastest',
+    sourceType: 'module'
+  },
+  rules: {
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'space-before-function-paren': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'no-unused-vars': 'warn',
+    'prettier/prettier': [
+      'error',
+      {
+        semi: false,
+        singleQuote: true,
+        useTabs: false,
+        tabWidth: 2,
+        trailingComma: 'none',
+        endOfLine: 'auto',
+        printWidth: 100
+      }
+    ],
+    'react/react-in-jsx-scope': 0,
+    'react/prefer-stateless-function': 0,
+    'react/jsx-filename-extension': 0,
+    'react/jsx-one-expression-per-line': 0,
+    'no-nested-ternary': 0,
+    'react/display-name': 'off'
+  },
+  ignorePatterns: ['/.cache', '/.git', '/.husky', '/.yarn', '/dist', '/queries']
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: [{ find: '@', replacement: resolve(__dirname, 'src') }]
+  },
   plugins: [react()]
 })
