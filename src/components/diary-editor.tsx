97fd02caeb40fdebfb10f476ee498d218e97e774
jjou33 <zanda33@naver.com>
2022-09-07 16:55:43 +0900

    FEAT: 소규모 기능 추가

---
+++ src/components/diary-editor.tsx
@@ -0,0 +1,54 @@
+import { useState } from 'react'
+
+const DiaryEditor = () => {
+  const [state, setState] = useState({
+    author: '',
+    content: '',
+    emotion: 1
+  })
+
+  const handleChangeState = (e: any) => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const handleSubmit = () => {
+    console.log(state)
+    alert('저장')
+  }
+  return (
+    <div className="DiaryEditor">
+      <h2>Diary Example</h2>
+      <div>
+        <input
+          name="author"
+          value={state.author}
+          onChange={handleChangeState}
+        />
+      </div>
+      <div>
+        <textarea
+          name="content"
+          value={state.content}
+          onChange={handleChangeState}
+        />
+      </div>
+      <div>
+        <select name="emotion" onChange={handleChangeState}>
+          <option value={1}>1</option>
+          <option value={2}>2</option>
+          <option value={3}>3</option>
+          <option value={4}>4</option>
+          <option value={5}>5</option>
+        </select>
+      </div>
+      <div>
+        <button onClick={handleSubmit}>일기 저장하기</button>
+      </div>
+    </div>
+  )
+}
+
+export default DiaryEditor
